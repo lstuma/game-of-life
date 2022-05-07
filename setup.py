f033c302d991d8185707acf2dedae68557aa68c7
Lukas <lukas.stockmann@t-online.de>
2022-05-07 13:33:45 +0200

    Dev(setup): added some necessary files

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_desc = fh.read()
+
+setuptools.setup(
+    name='Game Of Life',
+    version='0.1',
+    author='Lukas Stockmann',
+    author_email='g.lstuma@gmail.com',
+    description='A clone of the popular game called \'Game Of Life\'',
+    long_description=long_desc,
+    url='',
+    packages=setuptools.find_packages(),
+    install_requires=['setuptools,'
+                      'pygame',
+                      ],
+    python_requires='>=3.7',
+    entry_points={
+        'console_scripts': [
+            'game_of_life=game_of_life.game_of_life:main',
+        ]
+    }
+)
